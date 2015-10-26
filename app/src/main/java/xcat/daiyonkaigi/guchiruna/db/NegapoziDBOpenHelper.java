380ea8956fea63a23c640ed245d990b1fa7370e2
ryo_horigome <r15-horigome@xcat.co.jp>
2015-10-26 09:14:57 +0900

    ネガポジ画面追加

---
+++ app/src/main/java/xcat/daiyonkaigi/guchiruna/db/NegapoziDBOpenHelper.java
@@ -0,0 +1,32 @@
+package xcat.daiyonkaigi.guchiruna.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * SQLiteデータベースを生成するクラスです。
+ */
+public class NegapoziDBOpenHelper extends SQLiteOpenHelper {
+
+    public NegapoziDBOpenHelper(Context context) {
+        super(context, "guchiruna2", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //ネガポジ用のテーブル
+        db.execSQL("create table negapozi(" +
+                " id INTEGER PRIMARY KEY " +
+                ",Pozi REAR " +
+                ",Nega REAR " +
+                ",Year INTEGER " +
+                ",Month INTEGER " +
+                ",Day INTEGER " +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
